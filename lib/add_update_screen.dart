4118d83d6109cc7966f1541fbcb0395ddd33904d
Aldairtunq <201912945b@utea.edu.pe>
2022-12-24 12:05:57 -0500

    commit actualizar

--- lib/add_update_screen.dart
+++ lib/add_update_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:todo_app/db_handler.dart';
 import 'package:todo_app/model.dart';
@@ -15,6 +13,8 @@ class _AddUpdateTaskState extends State<AddUpdateTask> {
   DBHelper? dbHelper;
   late Future<List<TodoModel>> dataList;
 
+  final _fromkey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -28,6 +28,55 @@ class _AddUpdateTaskState extends State<AddUpdateTask> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final titleController = TextEditingController();
+    final descController = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Add/update/ Task",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.w600,
+            letterSpacing: 1,
+          ),
+        ),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(top: 100),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Form(
+                key: _fromkey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: 20),
+                      child: TextFormField(
+                        keyboardType: TextInputType.multiline,
+                        controller: titleController,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          hintText: "Note Title",
+                        ),
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Enter some text";
+                          }
+                          return null;
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
