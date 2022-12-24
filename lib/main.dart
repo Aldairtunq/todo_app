388ccbf7fbcfbafd8ade5a200cd2a93f9ee7edc3
Aldairtunq <201912945b@utea.edu.pe>
2022-12-24 02:33:49 -0500

    creacion delas  carpetas

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/home_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: " my aplicativo",
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.transparent,
+          foregroundColor: Colors.black,
+        ),
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
